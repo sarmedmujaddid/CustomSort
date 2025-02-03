621bb91d78e80f1f165300bc1b0b27a4f3fb1e27
Sarmad Mujaddid <sarmed.mujaddid@gmail.com>
2025-02-03 16:29:20 +0500

    :qInitial commit for alphanumeric sort algorithm

--- CustomSort.py
+++ CustomSort.py
@@ -1,16 +1,20 @@
 import re
 def custom_sort(string):
+
+    # Extract numbers as whole values and sort them
     numbers = re.findall(r'\d+', string)
     numbers = sorted(numbers, key=int)
    
+    # Extract and sort lowercase, uppercase, and other special characters
     lowercase = sorted([ch for ch in string if ch.islower()])
     uppercase = sorted([ch for ch in string if ch.isupper()])
     others = sorted([ch for ch in string if not ch.isalnum()])
+
+    # Combine sorted groups into the final string
     return "".join(numbers + lowercase + uppercase + others)
 
-# Example usage
-#input_str = "08s31a*&bDfK41BAl1"
+# Input & Output Formatting
 #input_str = "A11a4"
-input_str = input("Enter input: ")
+input_str = input("Enter Input: ")
 output_str = custom_sort(input_str)
-print(output_str)
+print(f"Output: {output_str}")
